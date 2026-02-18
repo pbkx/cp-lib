540583fef936044e2e4668a878b264a5a8a1e58b
steamproof <93405617+steamproof@users.noreply.github.com>
2026-02-17 22:42:58 -0800

    clear

--- bit/iterate_submasks.cpp
+++
@@ -1,8 +0,0 @@
-inline void iterate_submasks(int m, const function<void(int)>& f) {
-    for (int s = m; ; s = (s - 1) & m) {
-        f(s);
-        if (s == 0) {
-            break;
-        }
-    }
-}
